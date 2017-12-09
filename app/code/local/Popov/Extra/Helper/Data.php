7bef2ebb27dcbb391a3f8c12f3751f9af842d2ef
popovsergiy <popow.sergiy@gmail.com>
2017-12-09 22:35:11 +0200

    SKU field has been added

---
+++ app/code/local/Popov/Extra/Helper/Data.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @copyright   Copyright (c) 2014 http://agere.com.ua
+ * @author		Sergii Popov
+ * @license     http://opensource.org/licenses/gpl-license.php  GNU General Public License (GPL)
+ */
+class Popov_Extra_Helper_Data extends Mage_Core_Helper_Abstract {
+
+}
